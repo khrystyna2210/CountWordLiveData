ae14fc1b8a3d0c70ef03873aecb411bfc4c52327
Khrystyna <chrystyna2210@gmail.com>
2023-03-15 14:23:10 +0200

    Creating UI

--- app/src/main/java/com/example/countofwords/MainActivity.kt
+++ app/src/main/java/com/example/countofwords/MainActivity.kt
@@ -1,11 +1,27 @@
 package com.example.countofwords
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var etText: EditText
+    private lateinit var btnSave: Button
+    private lateinit var rvList: RecyclerView
+
+    public fun init() {
+        etText = findViewById(R.id.etText)
+        btnSave = findViewById(R.id.btnSave)
+        rvList = findViewById(R.id.rvList)
+        rvList.setBackgroundColor(getResources().getColor(R.color.gray))
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        init()
     }
 }
